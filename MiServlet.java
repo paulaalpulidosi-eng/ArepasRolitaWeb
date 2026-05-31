94a81dcbae2560749c9db04e81f705027ab6cd71
paulaalpulidosi-eng <paulaal.pulidosi@ecci.edu.co>
2026-05-31 02:25:13 -0500

    Add files via upload

---
+++ MiServlet.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MiServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<h1>✔ Servlet funcionando</h1>");
+        out.println("<p>Arepas La Rolita</p>");
+    }
+}
